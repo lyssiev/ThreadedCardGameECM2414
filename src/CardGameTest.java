a4e32763044d9d34fd1ac37826d9e91b0d11e434
sylvimelia <ss1527@exeter.ac.uk>
2023-11-27 16:41:00 +0000

    CardGame Tests 1

--- src/CardGameTest.java
+++ src/CardGameTest.java
@@ -13,23 +13,58 @@ class CardGameTest {
 
     //@Test
     //void setUpGame() {
+    //StringWriter output = new StringWriter();
+    //String input = "3 \n" + "pack1.txt \n";
     //}
 
     @Test
     void makeDecks() {
-        StringWriter output = new StringWriter();
-        String input = "3";
+        CardGame testGame = new CardGame();
+        testGame.makeDecks();
+        assert(testGame.decks != null);
+
     }
 
     @Test
     void makePlayers() {
+        CardGame testGame = new CardGame();
+        boolean[] testFlags = {false, false, false};
+        testGame.flags = testFlags;
+        testGame.numberOfPlayers = 3;
+        testGame.makeDecks();
+        testGame.makePlayers();
+        Player testPlayer = testGame.players.get(0);
+        assert(testGame.players != null && testPlayer.getDropDeck() == testGame.decks.get(1) && testPlayer.getDrawDeck() == testGame.decks.get(0));
     }
 
     @Test
     void dealHands() {
+        CardGame testGame = new CardGame();
+        boolean[] testFlags = {false, false, false};
+        testGame.flags = testFlags;
+        testGame.numberOfPlayers = 3;
+        testGame.makeDecks();
+        testGame.makePlayers();
+
+        testGame.dealHands();
+
+        assert(testGame.players.get(0).getHand().size() == 4);
+
     }
 
     @Test
     void dealDecks() {
+        CardGame testGame = new CardGame();
+        boolean[] testFlags = {false, false, false};
+        testGame.flags = testFlags;
+        testGame.numberOfPlayers = 3;
+        testGame.makeDecks();
+        testGame.makePlayers();
+        testGame.dealHands();
+        testGame.dealDecks();
+
+        assert(testGame.decks.get(0).getDeck().size() == 4);
     }
+
+
 }
